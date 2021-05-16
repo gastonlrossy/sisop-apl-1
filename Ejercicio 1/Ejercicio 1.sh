f8bd50b2d6f5b61e9bc98189594b644f7f724b20
gastonlrossy <gastonlrossy@gmail.com>
2021-05-16 15:34:16 -0300

    fix: Ejercicio 1 full.

--- Ejercicio 1/Ejercicio 1.sh
+++ Ejercicio 1/Ejercicio 1.sh
@@ -2,12 +2,12 @@
 
 
 #####                   APL N1                  #####
-#####		        Ejercicio 1 - Entrega       #####
-#####				APL1-ejercicio1.sh          #####
+#####		    Ejercicio 1 - Entrega           #####
+#####				Ejercicio 1.sh              #####
 
-#####			GRUPO Nº2                       #####
-#####       Tebes, Leandro - 40.227.531	        #####
-#####       Rossy, Gaston L. - 40.137.778       #####
+#####			      GRUPO Nº2                 #####
+#####         Tebes, Leandro - 40.227.531       #####
+#####         Rossy, Gaston L. - 40.137.778     #####
 #####	      Zella, Ezequiel - 41.915.248      #####
 #####       Cencic, Maximiliano - 41.292.382    #####
 #####       Bonvehi, Sebastian - 40.538.404     ##### 
@@ -17,32 +17,35 @@
 
 validarSintaxis() {
     echo "Error. La sintaxis esperada del script es la siguiente:"
-    echo $0 "<directorio> <cantidad>"
+    echo "$0" "<directorio> <cantidad>"
     echo "En lugar de eso, se recibio: "
-    echo $0 $*
+    echo "$0" "$*"
     exit
 }
 
 # Si el param 1 es un directorio invalido, 
 #  se imprime por stdout que no lo es.
 validarDirectorio() {
-    echo "Error. $1 no es un directorio valido."
+    echo "Error." "$1" "no es un directorio valido."
     exit
 }
 
 # En esta funcion se realizan las validaciones
 #  pertinentes para el script.
 validarParams() {
-    if test $1 -ne 2 ; then
-        validarSintaxis $2 $3 $4 $5 $6
+    #Validamos que la cantidad de parámetros que nos enviaron sea igual a 2.
+    if test "$1" -ne 2 ; then
+        validarSintaxis "$2" "$3" "$4" "$5" "$6"
     fi
 
-    if [[ ! -d $2 ]]; then
-        validarDirectorio $2
+    #Validamos que el parámetro 1 sea un directorio.
+    if [[ ! -d "$2" ]]; then
+        validarDirectorio "$2"
     fi
-    
-    if ! [[ $3 =~ ^[0-9]$ ]]; then
-        validarNumero $3
+
+    #Validamos que el parámetro 2 sea un número.
+    if ! [[ "$3" =~ ^[0-9]+$ ]]; then
+        validarNumero "$3"
     fi
 }
 
@@ -53,26 +56,46 @@ validarNumero() {
     exit
 }
 
-validarParams $# $1 $2 $3 $4 $5
-LIST=$(ls -d $1*/)
-ITEMS=()
-for d in $LIST; do            # En este for agregan a la lista ITEMS los directorios con la cantidad de directorios al comienzo
-    ITEM="`ls $d | wc -l`-$d" # con el formato <cantidad de directorios>-<directorio>
-    ITEMS+=($ITEM)
-done
+validarParams "$#" "$1" "$2" "$3" "$4" "$5"
 
+#Listamos los directorios
+LIST=$(ls -d "$1"*/)
+
+#Creamos un array vacio.
+ITEMS=()
+IFS=$'\n'
+#Agregamos a la lista ITEMS los directorios con la cantidad de directorios al comienzo.
+for d in ${LIST}; do
+    ITEM="$(ls "$d" | wc -l)-'$d'"  # con el formato <cantidad de directorios>-<directorio>    
+    ITEMS+=("$ITEM")
+done
 IFS=$'\n'
 
-sorted=($(sort -rV -t '-' -k 1 <<<${ITEMS[*]})) # Se ordena la lista de reversa
-CANDIDATES="${sorted[*]:0:$2}" # Se mete en CANDIDATES la lista previamente ordenada desde el comienzo hasta el numero ingresado como parametro 2
+# Se ordena la lista de reversa
+sorted=("$(sort -rV -t '-' -k 1 <<< "${ITEMS[*]}")")
+
+# Se mete en CANDIDATES la lista previamente ordenada desde el comienzo hasta el numero ingresado 
+# en el segundo parametro.
+CANDIDATES="${sorted[*]:0:$2}"
+
+#echo "candidatos: ${CANDIDATES[*]}"
+#exit 0
+
 unset IFS
 echo "La lista de directorios ordenada de forma descendente es: " # COMPLETAR
-printf "%s\n" "$(cut -d '-' -f 2 <<<${CANDIDATES[*]})" # Se imprimen por stdout los items en CANDIDATES eliminando la primera parte, osea lo previo al guion.
+echo
+# Se imprimen por stdout los directorios de la variable CANDIDATES.
+printf "%s\n" "$(cut -d '-' -f 2- <<< "${CANDIDATES[*]}")"
 
 
-# 1. ¿Cuál es el objetivo de este script?, ¿Qué parámetros recibe? 
+# 1.a ¿Cuál es el objetivo de este script?
 
-# Analiza los directorios en la raíz del script y en base al 1er parámetro lista las carpetas cuyo nombre sea el directorio del 1er parámetro + cualquier continuación. Esta lista se ordena según los archivos que tengan dentro de mayor a menor. Luego, en base al 2do parámetro, se delimita la cantidad de carpetas a mostrar en la salida final.
+# Imprime por consola los directorios subyacentes al directorio enviado en el primer parámetro del script,
+# ordenando la impresión por la cantidad de archivos y carpetas que contiene cada directorio de la lista
+# de mayor a menor.
+# La cantidad de directorios impresos es limitada por el segundo parámetro del script.
+
+# 1.b ¿Qué parámetros recibe? 
 
 # Recibe dos parámetros, el primero debe ser un directorio válido y el 2do debe ser un número.
 
@@ -88,20 +111,26 @@ printf "%s\n" "$(cut -d '-' -f 2 <<<${CANDIDATES[*]})" # Se imprimen por stdout 
 
 # Se agregó una validación para el 2do parámetro que tiene que ser un número válido.
 # Se agregó el parámetro al llamar la funcB (ahora “validarDirectorio”).
-# Al final de cada función que hacía un echo para comentar sobre el error, se seguía ejecutando el script en lugar de hacer un exit.
+
+# Errores:
+
+# No se llamaba la funcA en ningún lado.
+# Cuando se realiza la iteracion, no se estaba teniendo en cuenta a nombre de directorios con espacios.
+# Cuando se imprimian los directorios, no se estaba teniendo en cuenta si un directorio poseía un "-".
+# Si los parámetros enviados no eran correctos, no existía un exit que terminara el script.
 
 # 6. ¿Qué información brinda la variable $#? ¿Qué otras variables similares conocen? 
 # Explíquelas. 
 
 # $#: Cantidad de parámetros enviados
 
-# $1 ... $n: Parámetros enviados
+# $1 ... $n: Contenido del parámetro n.
 
-# $@ o $*: La lista de todos los parámetros
+# $@ o $*: La lista de todos los parámetros.
 
-# $$: El Pid del proceso actual
+# $$: El Pid del proceso actual.
 
-# $!: El Pid del último proceso hijo ejecutado en segundo plano
+# $!: El Pid del último proceso hijo ejecutado en segundo plano.
 
 # $?: Valor de ejecución del último comando
 
