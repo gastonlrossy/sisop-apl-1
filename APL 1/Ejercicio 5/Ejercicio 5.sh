754278cbea0d912b3d3a57ff71076f198bdb230a
gastonlrossy <gastonlrossy@gmail.com>
2021-05-18 01:42:48 -0300

    fix: Ejercicio 5 full

--- APL 1/Ejercicio 5/Ejercicio 5.sh
+++ APL 1/Ejercicio 5/Ejercicio 5.sh
@@ -2,26 +2,28 @@
 
 
 #####                  APL N1                   #####
-#####                Ejercicio 5                #####
-#####             APL1-ejercicio5.sh            #####
+#####		         Ejercicio 5                #####
+#####			  Ejercicio 5.sh    	        #####
 
-#####      GRUPO:                               #####
-#####       Tebes, Leandro - 40.227.531         #####
+#####	             GRUPO N°2                  #####
+#####       Tebes, Leandro - 40.227.531	        #####
 #####       Rossy, Gaston L. - 40.137.778       #####
-#####       Zella, Ezequiel - 41.915.248        #####
+#####	    Zella, Ezequiel - 41.915.248        #####
 #####       Cencic, Maximiliano - 41.292.382    #####
 #####       Bonvehi, Sebastian - 40.538.404     #####
 
 
 help(){
     echo "Hola!"
-    echo "Es necesario ejecutar este script teniendo las bibliotecas JO y JQ en las versiones 1.4 y 1.6 respectivamente."
-    echo "Al ejecutar el script, el mismo evaluará si se tienen dichas dependencias y de tenerlas, evaluará las versiones de las mismas."
-    printf "De no tenerlas o de tener una version anterior, el programa intentará instalarlas preguntandole al usuario previamente.\n\n"
+    echo "Es necesario ejecutar este script teniendo la biblioteca JQ y JO en las version 1.6 y 1.4 respectivamente."
+    echo "Al ejecutar el script, el mismo evaluará si se tiene dicha dependencia y de tenerla, evaluará las version de la misma."
+    printf "De no tenerla o de tener una version anterior, el programa intentará instalarla preguntandole al usuario previamente.\n\n"
+    echo "El objetivo del script es procesar las notas guardadas en archivos CSV y generar un archivo JSON con dichas notas a partir de todos los CSV guardados."
+    echo "El script recibirá los siguientes parámetros:"
+    echo " • --notas: Directorio en el que se encuentran los archivos CSV."
+    echo " • --salida: Ruta del archivo JSON a generar incluyendo el nombre del archivo."
     echo "Luego, la ejecucion del script es la siguiente:"
     printf "\t$0 --notas <directorio con los archivos .CSV> --salida <directorio + nombre del archivo json a generar>\n\n"
-
-<<<<<<< HEAD
     exit
 }
 
@@ -29,25 +31,15 @@ helpError() {
     echo "Hola!"
     printf "La sintaxis para la ejecución del script no fue correcta.\n"
     if [ "$1" != "" ]; then
-        printf "\nEl error fue: "$1"\n"
+        printf "\n"
+        echo El error fue: "$1"
+        printf "\n"
     fi
     printf "\nPodes ver la ayuda de la siguiente manera:"
     printf "\n\t$0 -h"
     printf "\n\t$0 -help"
     printf "\n\t$0 -?\n\n"
 
-=======
-#$1 # -> directorio con los .csv
-#$2 # -> ruta del archivo a json a generar
-
-#validar parámetros (existencia de rutas y permisos, cantidad de parámetros).   DONE
-clear
-if ! [ -d "$1" ]; then
-    echo "${1} no es un directorio valido."
-    exit
-elif ! [ -r "$1" ]; then
-    echo "${1} no posee posee permisos de lectura."
->>>>>>> 096834ac1c0a82bc84f272ed2f11a9c58e7f5255
     exit
 }
 
@@ -63,15 +55,15 @@ validacionParams(){
     fi
 
     if ! [ -d "$2" ]; then
-        helpError "${2} no es un directorio valido."
+        helpError "$2 no es un directorio valido."
     fi
 
     if ! [ -r "$2" ]; then
-        helpError "${2} no posee permisos de lectura."
+        helpError "$2 no posee permisos de lectura."
     fi
 
     if ! [ -d `dirname $4` ]; then
-        helpError "${4} no es un directorio valido."
+        helpError "$4 no es un directorio valido."
     fi
 }
 
@@ -81,12 +73,46 @@ obtenerNombreJson(){
     file="${1##*/}"
     extension="${file##*.}"
 
-    if [ -z "$extension" ]; then
-        helpError "No se ingreso el nombre del archivo a generar."
+    if [ -z "$pathSinExtension" ]; then
+        if [ -z "$extension" ]; then
+            helpError "No se ingreso el nombre del archivo a generar."
+        fi
+
+        pathSinExtension=$extension
     fi
 
     ruta=$pathSinExtension".json"
-    
+}
+
+validarBibliotecas() {
+    JQ_VERSION=`jq --version &> /dev/null`
+    if test $? -eq 0; then
+        JQ_VERSION=`jq --version`
+        if [[ $JQ_VERSION != *"1.6"* ]]; then
+            HAY_Q_INSTALAR_JQ=true
+        fi
+    else
+        HAY_Q_INSTALAR_JQ=true
+    fi
+
+    JO_VERSION=`jo -version &> /dev/null`
+    if test $? -eq "0"; then
+        JO_VERSION=`jo -version`
+        if [[ $JO_VERSION != *"1.4"* ]]; then
+            HAY_Q_INSTALAR_JO=true
+        fi
+    else
+        HAY_Q_INSTALAR_JO=true
+    fi
+
+    if [ "$HAY_Q_INSTALAR_JQ" = true ]; then
+        if [ "$HAY_Q_INSTALAR_JO" = true ]; then
+            helpError "No se puede ejecutar sin la biblioteca JQ en la version 1.6 y JO en la version 1.4"
+        fi
+        helpError "No se puede ejecutar sin la biblioteca JQ en la version 1.6"
+    elif [ "$HAY_Q_INSTALAR_JO" = true ]; then
+        helpError "No se puede ejecutar sin la biblioteca JO en la version 1.4"
+    fi
 }
 
 while getopts "?'help'h'-:" o; do
@@ -97,6 +123,8 @@ while getopts "?'help'h'-:" o; do
             validacionParams "$1" "$2" "$3" "$4"
 
             obtenerNombreJson "$4"
+
+            validarBibliotecas
 
         ;;
         *)
@@ -109,75 +137,8 @@ while getopts "?'help'h'-:" o; do
     esac
 done
 
-JQ_VERSION=`jq --version &> /dev/null`
-if test $? -eq 0; then
-    JQ_VERSION=`jq --version`
-    if [[ $JQ_VERSION != *"1.6"* ]]; then
-        HAY_Q_INSTALAR_JQ=true
-    fi
-else
-    HAY_Q_INSTALAR_JQ=true
-fi
-
-if [ "$HAY_Q_INSTALAR_JQ" = true ]; then
-    printf "\nEste script utiliza las librerias Jo y Jq.\nQuieres instalar Jq ahora? (Y/N) "
-    read OPCION
-    OPCION=$(echo ${OPCION} | tr [a-z] [A-Z])
-    while ([[ "$OPCION" != "Y" ]] && [[ "$OPCION" != "N" ]]); do
-        echo "Opcion invalida, vuelva a intentar.."
-        printf "\nEste script utiliza las librerias Jo y Jq.\nQuieres instalar Jq ahora? (Y/N) "
-        read OPCION
-        OPCION=$(echo ${OPCION} | tr [a-z] [A-Z])
-    done
-    if [[ "$OPCION" == "N" ]]; then
-        echo "Gracias, vuelva prontos."
-        exit
-    fi
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        printf "\n\nInstalando JQ para MacOs...\n"
-        brew install jq
-    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-        printf "\n\nInstalando JQ para Linux...\n"
-        sudo apt-get remove jq
-        sudo snap install jq --edge #para que sea la version 1.6, para la 1.5 no hace falta el --edge
-    fi
-fi
-
-JO_VERSION=`jo -version &> /dev/null`
-if test $? -eq "0"; then
-    JO_VERSION=`jo -version`
-    if [[ $JO_VERSION != *"1.4"* ]]; then
-        HAY_Q_INSTALAR_JO=true
-    fi
-else
-    HAY_Q_INSTALAR_JO=true
-fi
-
-if [ "$HAY_Q_INSTALAR_JO" = true ]; then
-    printf "\nEste script utiliza las librerias Jo y Jq.\nQuieres instalar Jo ahora? (Y/N) "
-    read OPCION
-    OPCION=$(echo ${OPCION} | tr [a-z] [A-Z])
-    while ([[ "$OPCION" != "Y" ]] && [[ "$OPCION" != "N" ]]); do
-        echo "Opcion invalida, vuelva a intentar.."
-        printf "\nEste script utiliza las librerias Jo y Jq.\nQuieres instalar Jo ahora? (Y/N) "
-        read OPCION
-        OPCION=$(echo ${OPCION} | tr [a-z] [A-Z])
-    done
-    if [[ "$OPCION" == "N" ]]; then
-        echo "Gracias, vuelva prontos."
-        exit
-    fi
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        printf "\n\nInstalando JO para MacOs...\n"
-        brew install jo
-    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-        printf "\n\nInstalando JO para Linux...\n"
-        sudo apt-get remove jo
-        sudo snap install jo
-    fi
-fi
+arrVacio=$( jq -n '{"actas": []}' )
+echo $arrVacio > "$ruta"
 
 for i in $(ls $2 | grep '\.csv$'); do
 MATERIA="${i%_*}"
@@ -202,19 +163,11 @@ resumen=$(awk '
   }' "$2/$i")
 
 
-  resumen=` echo $resumen | sed 's/\\n/\ /g'`
+resumen=` echo $resumen | sed 's/\\n/\ /g'`
 
-  IFS=' '
+IFS=' '
 
-  read -ra ARRAY <<<"$resumen"
-
-  json_vacio=$(jo -a < /dev/null)
-
-  echo $json_vacio > "$ruta"
-
-  actas_vacio=$( jo actas=:"$ruta" )
-
-  echo $actas_vacio > "$ruta"
+read -ra ARRAY <<<"$resumen"
   
 for (( c=0; c<${#ARRAY[*]}; c+=2 )); do
 
@@ -226,22 +179,18 @@ for (( c=0; c<${#ARRAY[*]}; c+=2 )); do
 
     if test $cantAparicionesDni -eq 0; then
       nuevoJson=$( jq --argjson alumno $obj_alumno \
-                      '.actas[.actas | length] += $alumno' "$ruta" )
-
+                      '.actas[.actas | length] += ($alumno)' "$ruta" )
     else
-      soloArrAlumnos=$( jq --argjson dni ${ARRAY[$c]} \
-                      --argjson nota_alumno "$obj_nota" \
-                      '.actas[] | select(.dni==$dni).notas += [$nota_alumno]' "$ruta" )
+        soloArrAlumnos=$( jq --argjson dni ${ARRAY[$c]} \
+                             --argjson nota_alumno "$obj_nota" \
+                                '.actas[] | select(.dni==$dni).notas += [$nota_alumno]' "$ruta" )
 
-    soloArrAlumnosFormateado=$( jq -s '.' <<< $soloArrAlumnos)
+        soloArrAlumnosFormateado=$( jq -s '.' <<< $soloArrAlumnos)
 
-    echo $soloArrAlumnosFormateado > $HOME"/arr_alumnos.json"
-
-    nuevoJson=$( jo actas=:$HOME"/arr_alumnos.json" )
-
+        nuevoJson=$( jq -n --argjson v "$soloArrAlumnosFormateado" '{"actas": ($v)}' )
     fi
 
-    echo $nuevoJson > "$ruta" # para guardar archivo final
+    echo $nuevoJson | jq '.' > "$ruta" # para guardar archivo final
 done
 
 done
