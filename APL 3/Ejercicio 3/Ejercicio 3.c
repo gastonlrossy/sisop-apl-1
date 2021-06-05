c67416081aa9d54e4b98510348866804ae6f6f6a
gastonlrossy <gastonlrossy@gmail.com>
2021-06-05 20:43:03 -0300

    feat: Ejercicio 3 apl 3

---
+++ APL 3/Ejercicio 3/Ejercicio 3.c
@@ -0,0 +1,20 @@
+/****                APL N°3                  *****/
+/****		      Ejercicio 1 - Entrega           *****/
+/****		         Ejercicio 1.sh               *****/
+
+/****		             GRUPO Nº2                *****/
+/****       Tebes, Leandro - 40.227.531       *****/
+/****       Rossy, Gaston L. - 40.137.778     *****/
+/****	      Zella, Ezequiel - 41.915.248      *****/
+/****       Cencic, Maximiliano - 41.292.382  *****/
+/****       Bonvehi, Sebastian - 40.538.404   *****/
+
+# Si la sintaxis esperada del script es invalida, 
+#  se imprime por stdout la salida esperada.
+
+#include <stdio.h>
+int main()
+{
+  printf ("Hello World!\n");
+  return 0;
+}
--- APL 3/Ejercicio 3/Ejercicio 3.sh
+++ APL 3/Ejercicio 3/Ejercicio 3.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
 
-#####                   APL N1                  #####
-#####		    Ejercicio 1 - Entrega           #####
-#####				Ejercicio 1.sh              #####
 
-#####			      GRUPO Nº2                 #####
-#####         Tebes, Leandro - 40.227.531       #####
-#####         Rossy, Gaston L. - 40.137.778     #####
-#####	      Zella, Ezequiel - 41.915.248      #####
-#####       Cencic, Maximiliano - 41.292.382    #####
-#####       Bonvehi, Sebastian - 40.538.404     ##### 
-
-# Si la sintaxis esperada del script es invalida, 
-#  se imprime por stdout la salida esperada.
 
 validarSintaxis() {
     echo "Error. La sintaxis esperada del script es la siguiente:"
